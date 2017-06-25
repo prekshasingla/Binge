eb5ad27c1869a8abac4631cbf78d3eeb349488a5
prakhar agarwal <prakharagarwal76@gmail.com>
2017-06-25 13:16:00 +0530

    created fragment and tablayout in viewpager

--- app/src/main/java/com/example/prakharagarwal/fingerlickingawesome/MainActivity.java
+++ app/src/main/java/com/example/prakharagarwal/fingerlickingawesome/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.prakharagarwal.fingerlickingawesome;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPagerAdapter mviewPagerAdapter;
+    ViewPager mviewPager;
+    TabLayout mtabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mviewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mviewPagerAdapter.addFragment(new DineoutFragment(), "Dineout");
+        mviewPager = (ViewPager) findViewById(R.id.viewpager);
+        mtabLayout = (TabLayout) findViewById(R.id.tablayout);
+
+        mviewPager.setAdapter(mviewPagerAdapter);
+        mtabLayout.setupWithViewPager(mviewPager);
     }
 }
