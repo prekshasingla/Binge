842564b169509f117619e67884614146dc5b3534
prakharagarwal76 <prakharagarwal76@gmail.com>
2017-07-23 13:13:27 +0530

    logo, ui changed

--- app/src/main/java/com/example/prakharagarwal/fingerlickingawesome/StoriesActivity.java
+++ app/src/main/java/com/example/prakharagarwal/fingerlickingawesome/StoriesActivity.java
@@ -4,6 +4,12 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 
 public class StoriesActivity extends FragmentActivity {
@@ -12,6 +18,8 @@ public class StoriesActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stories);
+
     }
+
 
 }
