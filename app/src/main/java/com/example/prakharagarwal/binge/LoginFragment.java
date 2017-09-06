a7b4fd01363dfcbd898d705f7997fbef7341d250
prakharagarwal76 <prakharagarwal76@gmail.com>
2017-09-06 22:51:54 +0530

    binge new splash logo

--- app/src/main/java/com/example/prakharagarwal/binge/LoginFragment.java
+++ app/src/main/java/com/example/prakharagarwal/binge/LoginFragment.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -54,6 +57,7 @@ public class LoginFragment extends Fragment implements
     private boolean passwordVerified=false;
     private boolean emailVerified=false;
     FirebaseDatabase mDatabase;
+    CheckBox showPassword;
 
     View rootView;
     public LoginFragment(){
@@ -67,7 +71,18 @@ public class LoginFragment extends Fragment implements
         ((LoginActivity) getActivity()).setActionBarTitle("Login");
 
         textViewError=(TextView) rootView.findViewById(R.id.login_error);
+        showPassword=(CheckBox)rootView.findViewById(R.id.fsu_show_password);
+        showPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(!b){
+                    user_password.setTransformationMethod(new PasswordTransformationMethod());
+                }else {
+                    user_password.setTransformationMethod(null);
 
+                }
+            }
+        });
         user_email=(EditText) rootView.findViewById(R.id.user_email);
         user_password=(EditText) rootView.findViewById(R.id.user_password);
         login=(Button)rootView.findViewById(R.id.login_button);
